ad2a848a7c4457c9d727b54ad7f8df2e2068f075
JoaSig <jksi@yousee.dk>
2016-02-24 03:36:30 +0100

    messing around with navbar

--- src/main/webapp/scripts/components/navbar/navbar.controller.js
+++ src/main/webapp/scripts/components/navbar/navbar.controller.js
@@ -3,11 +3,21 @@
 angular.module('documentmanagementApp')
     .controller('NavbarController', function ($scope, $location, $state, Auth, Principal, ENV) {
         $scope.isAuthenticated = Principal.isAuthenticated;
+        Principal.identity().then(function (account) {
+            $scope.account = account;
+        });
         $scope.$state = $state;
         $scope.inProduction = ENV === 'prod';
 
         $scope.logout = function () {
             Auth.logout();
+            $scope.getLoggedInUser();
             $state.go('home');
         };
+
+        $scope.getLoggedInUser = function () {
+            Principal.identity().then(function (account) {
+                $scope.account = account;
+            });
+        }
     });
