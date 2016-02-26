b0a5f7366a2d1895f24948a6a19f61a0929575ad
JoaSig <jksi@yousee.dk>
2016-02-26 11:32:48 +0100

    meter per hour

--- src/main/webapp/scripts/app/admin/dashboard/dashboard.controller.js
+++ src/main/webapp/scripts/app/admin/dashboard/dashboard.controller.js
@@ -68,6 +68,7 @@ angular.module('documentmanagementApp')
         $scope.getMachineForGraph = function () {
             $scope.machine($scope.drillingMachine);
             $scope.machinePerMinute($scope.drillMachine);
+            //$scope.machinePerDepth($scope.drillPerDepthMachine);
         };
 
 
@@ -147,7 +148,7 @@ angular.module('documentmanagementApp')
 
                 $scope.pieData = [
                     {
-                        key: "Meter per hour",
+                        key: "Meter per hour: " + drillingsByMachine.meterDrillingPerHour,
                         y: drillingsByMachine.meterDrillingPerHour
                     },
                     {
@@ -164,4 +165,97 @@ angular.module('documentmanagementApp')
                 }
             })
         };
+
+        //$scope.machinePerDepth = function () {
+        //    PileDrilling.machine({drillingMachine: $scope.drillPerDepthMachine}, function (drillingsByMachine) {
+        //        $scope.drilling = drillingsByMachine.machine;
+        //        $scope.drillingsByMachine = drillingsByMachine;
+        //        self.totalDepthMachine = drillingsByMachine.totalDrillingDepth;
+        //        self.totalMinMachine = drillingsByMachine.totalDrillTime;
+        //        function getHighestId(drillings) {
+        //            var highest = 0;
+        //            drillings.forEach(function (drilling) {
+        //                if (drilling.id > highest) {
+        //                    highest = drilling.id;
+        //                }
+        //            });
+        //            console.log('highest: ' + highest);
+        //            return highest;
+        //        }
+        //
+        //        function getLowestId(drillings) {
+        //            var lowest = 500;
+        //            drillings.forEach(function (drilling) {
+        //                if (drilling.id < lowest) {
+        //                    lowest = drilling.id;
+        //                }
+        //            });
+        //            console.log('lowest: ' + lowest);
+        //            return lowest;
+        //        }
+        //
+        //        if (drillingsByMachine.drillings.length) {
+        //            $scope.multiBarOptions = angular.copy(Chart.getMultiBarChartConfig());
+        //            $scope.multiBarOptions.title.text = drillingsByMachine.machine;
+        //            $scope.multiBarOptions.chart.yAxis.axisLabel = "Drilling Time (mm)";
+        //            $scope.multiBarOptions.chart.xDomain = [getLowestId(drillingsByMachine.drillings), getHighestId(drillingsByMachine.drillings)];
+        //            var drillingMinutes = [];
+        //            drillingsByMachine.drillings.forEach(function (drilling) {
+        //                drillingMinutes.push({
+        //                    x: drilling.drillingStartTime,
+        //                    y: drilling.projectDrillingDepth
+        //                });
+        //            });
+        //            $scope.pdData = [{
+        //                values: drillingMinutes,
+        //                key: 'DrillingMinutes',
+        //                color: '#673ab7'
+        //            }];
+        //        }
+        //
+        //
+        //        $scope.pieOptions = {
+        //            chart: {
+        //                type: 'pieChart',
+        //                height: 450,
+        //                donut: true,
+        //                x: function(d){return d.key;},
+        //                y: function(d){return d.y;},
+        //                showLabels: true,
+        //
+        //                pie: {
+        //                    startAngle: function(d) { return d.startAngle/2 -Math.PI/2 },
+        //                    endAngle: function(d) { return d.endAngle/2 -Math.PI/2 }
+        //                },
+        //                duration: 500,
+        //                legend: {
+        //                    margin: {
+        //                        top: 5,
+        //                        right: 70,
+        //                        bottom: 5,
+        //                        left: 0
+        //                    }
+        //                }
+        //            }
+        //        };
+        //
+        //        $scope.pieData = [
+        //            {
+        //                key: "Meter per hour",
+        //                y: drillingsByMachine.meterDrillingPerHour
+        //            },
+        //            {
+        //                key: "",
+        //                y: 10 - drillingsByMachine.meterDrillingPerHour
+        //            }
+        //        ];
+        //
+        //
+        //
+        //    }, function (response) {
+        //        if (response.status === 404) {
+        //            $scope.loadAll();
+        //        }
+        //    })
+        //};
     });
