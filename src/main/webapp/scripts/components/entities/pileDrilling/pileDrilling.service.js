eb74c3d712a35b7c3f96050bb7051ddbb7c5f7d3
JoaSig <jksi@yousee.dk>
2016-02-26 17:44:17 +0100

    removed immutableReport from ui

--- src/main/webapp/scripts/components/entities/pileDrilling/pileDrilling.service.js
+++ src/main/webapp/scripts/components/entities/pileDrilling/pileDrilling.service.js
@@ -4,32 +4,34 @@ angular.module('documentmanagementApp')
     .factory('PileDrilling', function ($resource, DateUtils) {
         return $resource('api/pileDrillings/:id', {}, {
             'query': { method: 'GET', isArray: true},
-            'machine': { method: 'GET', isArray: false, url: '/api/pileDrillings/machine/:drillingMachine'},
-            'machines': { method: 'GET', isArray: true, url: '/api/util/machine'},
             'get': {
                 method: 'GET',
                 transformResponse: function (data) {
                     data = angular.fromJson(data);
-                    console.log('data.drillingStartTime: ' + data.drillingStartTime);
-                    data.drillingStartTime = DateUtils.convertLocaleDateFromServer(data.drillingStartTime);
-                    data.drillingStartDate = DateUtils.convertLocaleDateFromServer(data.drillingStartDate);
-                    data.drillingEndDate = DateUtils.convertLocaleDateFromServer(data.drillingEndDate);
+                    data.StartDate = DateUtils.convertLocaleDateFromServer(data.StartDate);
+                    data.EndDate = DateUtils.convertLocaleDateFromServer(data.EndDate);
+                    data.StartTime = DateUtils.convertLocaleDateFromServer(data.StartTime);
+                    data.EndTime = DateUtils.convertLocaleDateFromServer(data.EndTime);
                     return data;
                 }
             },
             'update': {
                 method: 'PUT',
                 transformRequest: function (data) {
-                    data.drillingStartDate = DateUtils.convertLocaleDateToServer(data.drillingStartDate);
-                    data.drillingEndDate = DateUtils.convertLocaleDateToServer(data.drillingEndDate);
+                    data.StartDate = DateUtils.convertLocaleDateToServer(data.StartDate);
+                    data.EndDate = DateUtils.convertLocaleDateToServer(data.EndDate);
+                    data.StartTime = DateUtils.convertLocaleDateToServer(data.StartTime);
+                    data.EndTime = DateUtils.convertLocaleDateToServer(data.EndTime);
                     return angular.toJson(data);
                 }
             },
             'save': {
                 method: 'POST',
                 transformRequest: function (data) {
-                    data.drillingStartDate = DateUtils.convertLocaleDateToServer(data.drillingStartDate);
-                    data.drillingEndDate = DateUtils.convertLocaleDateToServer(data.drillingEndDate);
+                    data.StartDate = DateUtils.convertLocaleDateToServer(data.StartDate);
+                    data.EndDate = DateUtils.convertLocaleDateToServer(data.EndDate);
+                    data.StartTime = DateUtils.convertLocaleDateToServer(data.StartTime);
+                    data.EndTime = DateUtils.convertLocaleDateToServer(data.EndTime);
                     return angular.toJson(data);
                 }
             }
