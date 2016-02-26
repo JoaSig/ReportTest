d55e267e0a6e4a1a82a7e87cb399009ffe05ea61
JoaSig <jksi@yousee.dk>
2016-02-26 14:48:02 +0100

    removed immutableReport from ui

--- src/main/webapp/scripts/components/entities/immutableReport/immutableReport.search.service.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('documentmanagementApp')
-    .factory('ImmutableReportSearch', function ($resource) {
-        return $resource('api/_search/immutableReports/:query', {}, {
-            'query': { method: 'GET', isArray: true}
-        });
-    });
